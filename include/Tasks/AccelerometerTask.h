eeee8032a78347bf6bec34df94721c94a5080cbb
Sean <skelstar@gmail.com>
2024-06-28 11:02:58 +1200

    Connects to the onc device twice.

--- include/Tasks/AccelerometerTask.h
+++ include/Tasks/AccelerometerTask.h
@@ -1,86 +1,86 @@
-#pragma once
-#include <Arduino.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <elapsedMillis.h>
-#include <Types.h>
+// #pragma once
+// #include <Arduino.h>
+// #include <freertos/FreeRTOS.h>
+// #include <freertos/task.h>
+// #include <elapsedMillis.h>
+// #include <Types.h>
 
-#include <M5StickCPlus.h>
+// #include <M5StickCPlus.h>
 
-#include <I2C_MPU6886.h>
+// #include <I2C_MPU6886.h>
 
-namespace AccelerometerTask
-{
-    TaskHandle_t taskHandle = NULL;
-    const char *taskName = "AccelerometerTask";
+// namespace AccelerometerTask
+// {
+//     TaskHandle_t taskHandle = NULL;
+//     const char *taskName = "AccelerometerTask";
 
-    const float tapThreshold = 1.6;
-    const unsigned long doubleTapWindowL = 200;
-    const unsigned long doubleTapWindowH = 300;
+//     const float tapThreshold = 1.6;
+//     const unsigned long doubleTapWindowL = 200;
+//     const unsigned long doubleTapWindowH = 300;
 
-    // prototypes
-    bool isDoubleTap(elapsedMillis sinceLastTap);
-    void sendPacket(InputPacket *packet);
+//     // prototypes
+//     bool isDoubleTap(elapsedMillis sinceLastTap);
+//     void sendPacket(InputPacket *packet);
 
-    void task1(void *pvParameters)
-    {
-        elapsedMillis sinceLastTap = 0;
+//     void task1(void *pvParameters)
+//     {
+//         elapsedMillis sinceLastTap = 0;
 
-        InputPacket packet;
+//         InputPacket packet;
 
-        Serial.printf("%s: Started\n", taskName);
+//         Serial.printf("%s: Started\n", taskName);
 
-        vTaskDelay(100);
+//         vTaskDelay(100);
 
-        M5.Imu.Init();
+//         M5.Imu.Init();
 
-        while (1)
-        {
-            float ax, ay, az;
+//         while (1)
+//         {
+//             float ax, ay, az;
 
-            M5.IMU.getAccelData(&ax, &ay, &az);
+//             M5.IMU.getAccelData(&ax, &ay, &az);
 
-            if (ax > tapThreshold || ay > tapThreshold || az > tapThreshold)
-            {
-                if (isDoubleTap(sinceLastTap))
-                {
-                    Serial.printf("Double Tap!\n");
+//             if (ax > tapThreshold || ay > tapThreshold || az > tapThreshold)
+//             {
+//                 if (isDoubleTap(sinceLastTap))
+//                 {
+//                     Serial.printf("Double Tap!\n");
 
-                    packet.id++;
-                    packet.input = InputOption::ACCEL;
-                    packet.event = ButtonEvent::DOUBLE_TAP;
+//                     packet.id++;
+//                     packet.input = InputOption::ACCEL;
+//                     packet.event = ButtonEvent::DOUBLE_TAP;
 
-                    sendPacket(&packet);
-                }
-                sinceLastTap = 0;
-            }
+//                     sendPacket(&packet);
+//                 }
+//                 sinceLastTap = 0;
+//             }
 
-            vTaskDelay(TICKS_5ms);
-        }
-    }
+//             vTaskDelay(TICKS_5ms);
+//         }
+//     }
 
-    bool isDoubleTap(elapsedMillis sinceLastTap)
-    {
-        unsigned long lastTap = (unsigned long)sinceLastTap;
-        return lastTap > doubleTapWindowL && lastTap < doubleTapWindowH;
-    }
+//     bool isDoubleTap(elapsedMillis sinceLastTap)
+//     {
+//         unsigned long lastTap = (unsigned long)sinceLastTap;
+//         return lastTap > doubleTapWindowL && lastTap < doubleTapWindowH;
+//     }
 
-    void sendPacket(InputPacket *packet)
-    {
-        InputPacket *data;
-        data = packet;
-        xQueueSend(xInputsQueue, (void *)&data, TICKS_10ms);
-    }
+//     void sendPacket(InputPacket *packet)
+//     {
+//         InputPacket *data;
+//         data = packet;
+//         xQueueSend(xInputsQueue, (void *)&data, TICKS_10ms);
+//     }
 
-    void createTask(int stackDepth)
-    {
-        xTaskCreatePinnedToCore(
-            task1,
-            taskName,
-            /*stack depth*/ stackDepth,
-            /*params*/ NULL,
-            /*priority*/ 1,
-            &taskHandle,
-            /*core*/ 1);
-    }
-}
+//     void createTask(int stackDepth)
+//     {
+//         xTaskCreatePinnedToCore(
+//             task1,
+//             taskName,
+//             /*stack depth*/ stackDepth,
+//             /*params*/ NULL,
+//             /*priority*/ 1,
+//             &taskHandle,
+//             /*core*/ 1);
+//     }
+// }
